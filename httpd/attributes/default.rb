bfd0c480b0bf5eb1a79d65a6b9f2aaa55a8e0cd2
ChefDK User <msandler@spandox.com>
2017-09-05 21:05:31 +0000

    case statement

--- httpd/attributes/default.rb
+++ httpd/attributes/default.rb
@@ -1,4 +1,12 @@
+#require 'pry'
+#binding.pry
+
 default['httpd']['cookbookname']='httpd'
-default['httpd']['package_name']='httpd'
 default['httpd']['service_name']='httpd'
 default['httpd']['index_location']='/var/www/html/index.html'
+case node['platform']
+when 'ubuntu'
+  default['httpd']['package_name']='apache2'
+when 'centos'
+  default['httpd']['package_name']='httpd'
+end
--- httpd/spec/unit/recipes/install_spec.rb
+++ httpd/spec/unit/recipes/install_spec.rb
@@ -21,5 +21,21 @@ describe 'httpd::install' do
     it 'installs the corisponding package' do
       expect(chef_run).to install_package("#{chef_run.node[:httpd][:package_name]}")
     end
+  end  
+  context 'When all attributes are default, on an Ubuntu 14.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+    it 'installs the corisponding package' do
+      expect(chef_run).to install_package('apache2')
+    end
   end
+
 end
